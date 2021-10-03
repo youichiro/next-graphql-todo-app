aaa84b898cb1ae42c5576927420494fcd11fd724
youichiro <cinnamon416@gmail.com>
2021-10-03 20:16:50 +0900

    create seed

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import prisma from '../lib/prisma';
+
+const main = async () => {
+  const user = await prisma.user.upsert({
+    where: { email: 'seed@prisma.io' },
+    update: {},
+    create: {
+      name: 'bob',
+      email: 'seed@prisma.io',
+      projects: {
+        create: [
+          {
+            name: 'sample project',
+            tasks: {
+              create: [
+                {
+                  title: 'sample task 1',
+                  description: 'hoge huga piyo',
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  })
+}
+
+main()
+.catch((e) => {
+  console.error(e)
+  process.exit(1)
+})
+.finally(async () => {
+  await prisma.$disconnect()
+})
