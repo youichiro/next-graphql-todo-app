44b39c86bcdfde17f691cc1af4b446d1905ebf2b
youichiro <cinnamon416@gmail.com>
2021-10-04 00:53:20 +0900

    define apollo client and query from frontend

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import { ApolloProvider } from '@apollo/client'
+import apolloClient from '../lib/apollo'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
+}
+
+export default MyApp
