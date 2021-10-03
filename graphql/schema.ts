4420970233ab1a1865022488c13439086abf014f
youichiro <cinnamon416@gmail.com>
2021-10-04 00:22:54 +0900

    use Nexus makeSchema

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,12 +1,14 @@
-import { gql } from 'apollo-server-micro'
+import { makeSchema } from 'nexus'
+import { join } from 'path'
 
-export const typeDefs = gql`
-  type Project {
-    id: Int
-    name: String
-  }
-
-  type Query {
-    projects: [Project]!
-  }
-`
+export const schema = makeSchema({
+  types: [],
+  outputs: {
+    typegen: join(process.cwd(), 'node_modules', '@types', 'nexus-typegen', 'index.d.ts'),
+    schema: join(process.cwd(), 'graphql', 'schema.graphql'),
+  },
+  contextType: {
+    export: 'Context',
+    module: join(process.cwd(), 'graphql', 'context.ts'),
+  },
+})
