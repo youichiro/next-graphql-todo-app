fe727f6c7767db9b01f27dd9e0fa866e78ef97a7
youichiro <cinnamon416@gmail.com>
2021-10-06 09:10:23 +0900

    srcに移動

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import prisma from '../lib/prisma';
+import prisma from '../src/lib/prisma';
 
 const main = async () => {
   const user = await prisma.user.upsert({
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -6,10 +6,10 @@ export const schema = makeSchema({
   types,
   outputs: {
     typegen: join(process.cwd(), 'node_modules', '@types', 'nexus-typegen', 'index.d.ts'),
-    schema: join(process.cwd(), 'graphql', 'schema.graphql'),
+    schema: join(process.cwd(), 'src', 'graphql', 'schema.graphql'),
   },
   contextType: {
     export: 'Context',
-    module: join(process.cwd(), 'graphql', 'context.ts'),
+    module: join(process.cwd(), 'src', 'graphql', 'context.ts'),
   },
 })
