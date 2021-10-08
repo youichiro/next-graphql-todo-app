2212a6a88a107d08eed8f5454bac311de369d338
youichiro <cinnamon416@gmail.com>
2021-10-08 09:46:00 +0900

    emotion/cacheを導入

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,34 @@
+import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import createEmotionCache from '../components/createEmotionCache'
+import createEmotionServer from '@emotion/server/create-instance'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage
+    const cache = createEmotionCache()
+    const { extractCriticalToChunks } = createEmotionServer(cache)
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) => (props) => <App emotionCache={cache} {...props} />
+      })
+
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        data-emotion={`${style.key} ${style.ids.join(' ')}`}
+        key={style.key}
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    ));
+
+    return {
+      ...initialProps,
+      styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
+    }
   }
 
   render() {
