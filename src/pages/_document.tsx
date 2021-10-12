021477b635a4bb2cbc6f89d530d2edb34461c91d
youichiro <cinnamon416@gmail.com>
2021-10-12 23:02:25 +0900

    npm remove @emotion/cache

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,34 +1,11 @@
-import createEmotionServer from '@emotion/server/create-instance';
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import React from 'react';
-import createEmotionCache from '../components/createEmotionCache';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const originalRenderPage = ctx.renderPage;
-    const cache = createEmotionCache();
-    const { extractCriticalToChunks } = createEmotionServer(cache);
-
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App: any) => (props) => <App emotionCache={cache} {...props} />,
-      });
-
     const initialProps = await Document.getInitialProps(ctx);
-    const emotionStyles = extractCriticalToChunks(initialProps.html);
-    const emotionStyleTags = emotionStyles.styles.map((style) => (
-      <style
-        data-emotion={`${style.key} ${style.ids.join(' ')}`}
-        key={style.key}
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{ __html: style.css }}
-      />
-    ));
 
-    return {
-      ...initialProps,
-      styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
-    };
+    return { ...initialProps };
   }
 
   render() {
