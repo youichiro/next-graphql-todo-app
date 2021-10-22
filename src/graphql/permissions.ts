17029cb2ab69c4aaf32e92585ac725513ecf79a2
youichiro <cinnamon416@gmail.com>
2021-10-22 23:50:37 +0900

    QueryにRuleを設定

--- src/graphql/permissions.ts
+++ src/graphql/permissions.ts
@@ -1,4 +1,4 @@
-import { rule, shield } from 'graphql-shield';
+import { allow, and, deny, rule, shield } from 'graphql-shield';
 import { getSession } from 'next-auth/client';
 
 const isAuthenticated = rule({ cache: 'contextual' })(async (_parent, _args, { req }, _info) => {
@@ -9,11 +9,23 @@ const isAuthenticated = rule({ cache: 'contextual' })(async (_parent, _args, { r
   return Boolean(session);
 });
 
+const isValidUserId = rule({ cache: 'contextual' })(async (_parent, args, { req }, _info) => {
+  if (process.env.NODE_ENV !== 'production') {
+    return true;
+  }
+  const session = await getSession({ req });
+  return session && args.userId === session.userId
+})
+
 export const permissions = shield({
   Query: {
-    '*': isAuthenticated,
+    '*': deny,
+    projects: and(isAuthenticated, isValidUserId),
+    selectedProject: and(isAuthenticated, isValidUserId),
   },
   Mutation: {
     '*': isAuthenticated,
   },
+}, {
+  fallbackRule: allow,
 });
