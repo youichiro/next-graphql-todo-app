cd7540d4359df3c160157728cc3ebc2d82230da0
youichiro <cinnamon416@gmail.com>
2021-10-24 16:22:43 +0900

    とりあえずテスト通った

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,17 +2,24 @@
  * @jest-environment jsdom
  */
 
-import { render, screen } from '@testing-library/react'
-import Home from '../src/pages'
+import { render, screen, waitFor } from '@testing-library/react';
+import client from 'next-auth/client';
+import Home from '../src/pages';
+jest.mock('next-auth/client');
 
 describe('HOME', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+  it('renders a heading', async () => {
+    const mockSession = {
+      expires: '1',
+      user: { email: 'a', name: 'a', image: 'a' },
+    };
 
-    const heading = screen.getByRole('heading', {
-      name: 'HOGE'
+    (client.useSession as jest.Mock).mockReturnValueOnce([mockSession, true]);
+
+    render(<Home />);
+
+    await waitFor(() => {
+      expect(screen.getByText('Validation session...'));
     })
-
-    expect(heading).toBeInTheDocument()
-  })
-})
+  });
+});
