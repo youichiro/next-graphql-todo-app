333fa0292405075d88229effaedaefe6241778e5
youichiro <cinnamon416@gmail.com>
2021-10-30 08:00:21 +0900

    フォーム

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,11 +1,25 @@
-import { Box, Input, FormControl } from "@chakra-ui/react";
+import { Box, Input, FormControl, Button, Stack } from '@chakra-ui/react';
+import { Formik, Field, Form } from 'formik';
 
 const TaskForm: React.FC = () => {
   return (
     <Box p='16px'>
-      <FormControl id="task">
-        <Input type='text' placeholder="Add task" />
-      </FormControl>
+      <Formik initialValues={{ taskName: '' }} onSubmit={() => null}>
+        <Form>
+          <Stack direction='row' alignItems='center'>
+            <Field name='taskName'>
+              {({ field }) => (
+                <FormControl>
+                  <Input {...field} id='taskName' type='text' placeholder='Add task' />
+                </FormControl>
+              )}
+            </Field>
+            <Button colorScheme='teal' type='submit'>
+              Submit
+            </Button>
+          </Stack>
+        </Form>
+      </Formik>
     </Box>
   );
 };
