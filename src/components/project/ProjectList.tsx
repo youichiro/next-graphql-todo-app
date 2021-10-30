674f8350a95242127a74eac6e46d6d5a6dcadbcc
youichiro <cinnamon416@gmail.com>
2021-10-30 22:49:51 +0900

    fix

--- src/components/project/ProjectList.tsx
+++ src/components/project/ProjectList.tsx
@@ -1,19 +1,20 @@
-import { List, Box } from '@chakra-ui/react';
+import { List, Box, Text } from '@chakra-ui/react';
 import ProjectListItem from './ProjectListItem';
 import { Project } from '.prisma/client';
 
-
 type Props = {
-  projects: Project[]
-  selectedProjectId?: number
-  handleClick: (projectId: number) => void
-}
+  projects: Project[];
+  selectedProjectId?: number;
+  handleClick: (projectId: number) => void;
+};
 
 const ProjectList: React.FC<Props> = ({ projects, selectedProjectId, handleClick }) => {
   if (projects.length === 0) {
     return (
-      <Box><Text color='gray'>Select project.</Text></Box>
-    )
+      <Box>
+        <Text color='gray'>Select project.</Text>
+      </Box>
+    );
   }
   return (
     <Box>
