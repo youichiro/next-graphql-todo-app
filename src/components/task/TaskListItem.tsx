184e94b605c7f7bac3283396dad3f6847376dc51
youichiro <cinnamon416@gmail.com>
2021-10-30 16:45:21 +0900

    fix

--- src/components/task/TaskListItem.tsx
+++ src/components/task/TaskListItem.tsx
@@ -1,17 +1,29 @@
 import { ListItem, Checkbox } from '@chakra-ui/react';
+import { useContext } from 'react';
+import { TaskContext } from './TaskContainer';
 import { Task } from '.prisma/client';
 
 type Props = {
   task: Task;
-  setSelectedTask: (task: Task) => void
+  setSelectedTask: (task: Task) => void;
 };
 
 const TaskListItem: React.FC<Props> = ({ task, setSelectedTask }) => {
+  const { selectedTask } = useContext(TaskContext);
+
   const handleClick = (task: Task) => {
     setSelectedTask(task);
   };
+
   return (
-    <ListItem key={task.id} px='16px' py='8px' onClick={() => handleClick(task)}>
+    <ListItem
+      key={task.id}
+      px='16px'
+      py='8px'
+      onClick={() => handleClick(task)}
+      bg={selectedTask?.id === task.id ? 'gray.100' : ''}
+      borderRadius='8px'
+    >
       <Checkbox colorScheme='teal' isChecked={task.done} color={!task.title ? 'gray' : ''}>
         {task.title || 'untitled'}
       </Checkbox>
