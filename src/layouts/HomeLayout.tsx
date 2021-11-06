770f012fae31c77a2b618d97a7d882c9c7e6a36c
youichiro <cinnamon416@gmail.com>
2021-11-06 23:13:02 +0900

    fix

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,9 +1,7 @@
 import { Box, Flex } from '@chakra-ui/react';
 import Account from '../components/account/Account';
 import ProjectContainer from '../components/project/ProjectContainer';
-import ProjectList from '../components/project/ProjectList';
 import TaskContainer from '../components/task/TaskContainer';
-import TaskDetail from '../components/task/TaskDetail';
 
 const HomeLayout: React.FC = () => {
   return (
