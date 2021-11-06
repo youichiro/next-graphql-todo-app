c78b1623a7b2c915437bb0db1f0f4aa27e1169ce
youichiro <cinnamon416@gmail.com>
2021-11-07 06:35:40 +0900

    DeleteTaskMutationを分離

--- src/components/task/TaskContainer.tsx
+++ src/components/task/TaskContainer.tsx
@@ -1,8 +1,7 @@
-import { useMutation, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { Box, Flex, Stack, Text } from '@chakra-ui/react';
 import { createContext, useContext, useState } from 'react';
 import { sortTask, filterIncomplateTasks, filterComplateTasks } from '../../functional/tasks';
-import { DeleteTask } from '../../graphql/mutations';
 import { SelectedProjectQuery } from '../../graphql/queries';
 import { SessionContext } from '../../pages';
 import Loading from '../common/Loading';
@@ -24,25 +23,10 @@ const TaskContainer: React.FC = () => {
   const query = useQuery(SelectedProjectQuery, {
     variables: { userId: session.userId },
   });
-  const [deleteTask, mutation3] = useMutation(DeleteTask, {
-    refetchQueries: [SelectedProjectQuery],
-  });
 
   if (query.loading) return <Loading />;
   if (query.error) return <p>Error... {query.error.message}</p>;
   if (!query.data.selectedProject) return <p>Select your project.</p>;
-  if (mutation3.error) return <p>Submission error! {mutation3.error.message}</p>;
-
-  const handleTaskDelete = () => {
-    if (selectedTask) {
-      deleteTask({
-        variables: {
-          id: selectedTask.id,
-        },
-      });
-      setSelectedTask(null);
-    }
-  };
 
   const tasks = sortTask(query.data.selectedProject.project.tasks);
   const incomplateTasks = filterIncomplateTasks(tasks);
@@ -54,23 +38,16 @@ const TaskContainer: React.FC = () => {
         <Stack flex='1' spacing='24px' px='16px' my='32px' overflow='scroll'>
           <TaskProjectHeading projectName={query.data.selectedProject.project.name} />
           <TaskCreateForm projectId={query.data.selectedProject.project.id} />
-          <TaskList
-            tasks={incomplateTasks}
-            setSelectedTask={setSelectedTask}
-          />
+          <TaskList tasks={incomplateTasks} setSelectedTask={setSelectedTask} />
           <Box pt='32px' hidden={complateTasks.length === 0}>
             <Text color='gray'>Complated</Text>
           </Box>
-          <TaskList
-            tasks={complateTasks}
-            setSelectedTask={setSelectedTask}
-            color='gray'
-          />
+          <TaskList tasks={complateTasks} setSelectedTask={setSelectedTask} color='gray' />
         </Stack>
         <Stack flex='1' borderLeft='solid 1px whitesmoke' px='16px' py='32px' spacing='16px'>
           <TaskDetail selectedTask={selectedTask} setSelectedTask={setSelectedTask} />
           <Box borderTop='solid 1px whitesmoke' textAlign='right' hidden={!selectedTask}>
-            <TaskDeleteButton handleTaskDelete={handleTaskDelete} />
+            <TaskDeleteButton taskId={selectedTask?.id} setSelectedTask={setSelectedTask} />
           </Box>
         </Stack>
       </Flex>
--- src/components/task/TaskDeleteButton.tsx
+++ src/components/task/TaskDeleteButton.tsx
@@ -1,17 +1,31 @@
+import { useMutation } from '@apollo/client';
 import { DeleteIcon } from '@chakra-ui/icons';
 import { Box, IconButton } from '@chakra-ui/react';
+import { Task } from '@prisma/client';
+import { DeleteTask } from '../../graphql/mutations';
+import { SelectedProjectQuery } from '../../graphql/queries';
 
 type Props = {
-  handleTaskDelete: () => void;
+  taskId: number | null;
+  setSelectedTask: (task: Task) => void;
 };
 
-const TaskDeleteButton: React.FC<Props> = ({ handleTaskDelete }) => {
+const TaskDeleteButton: React.FC<Props> = ({ taskId, setSelectedTask }) => {
+  const [deleteTask, mutation] = useMutation(DeleteTask, {
+    refetchQueries: [SelectedProjectQuery],
+  });
+
   const handleClick = () => {
-    const check = window.confirm('Delete this task?')
+    const check = window.confirm('Delete this task?');
     if (check) {
-      handleTaskDelete()
+      deleteTask({ variables: { id: taskId } });
+      setSelectedTask(null);
     }
-  }
+  };
+
+  if (!taskId) return <></>
+  if (mutation.error) return <p>Submission error! {mutation.error.message}</p>;
+
   return (
     <Box mx='16px' mt='8px'>
       <IconButton
